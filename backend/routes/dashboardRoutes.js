e86fb62ab82135cb02a2328832dce19cb9db7c4e
akashzone <akashnadar.dev@gmail.com>
2026-06-05 16:05:44 +0530

    Fix dashboard controller typo (rename dashboardConroller.js to dashboardController.js)

--- backend/routes/dashboardRoutes.js
+++ backend/routes/dashboardRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const authMiddleware = require("../middleware/authMiddleware.js");
-const dashboardSummary = require("../controller/dashboardConroller.js")
+const dashboardSummary = require("../controller/dashboardController.js")
 
 router.get("/summary", authMiddleware,dashboardSummary);
 
